d07fe58ecba075f246baf2c1db3ac8c3c9bc5d16
Fernando Batista <118860248+fernandoTbatista@users.noreply.github.com>
2022-11-26 06:33:50 -0300

    Create DashboardController.php
    atu

---
+++ App/Controller/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+class DashboardController extends Controller{
+
+    public static function index()
+    {
+        parent::isProtected();
+
+        // acessar...
+
+        include PATH_VIEW . 'home.php';
+    }
+}
