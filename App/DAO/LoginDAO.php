dd8c1883d387ac4545355f9d5ad6c154ffe4810a
Fernando Batista <118860248+fernandoTbatista@users.noreply.github.com>
2022-11-27 19:53:35 -0300

    Update LoginDAO.php

--- App/DAO/LoginDAO.php
+++ App/DAO/LoginDAO.php
@@ -9,6 +9,19 @@ class LoginDAO extends DAO
         parent:: __construct();
     }
 
+    public function setNewPassWordForUserByEmail($email, $newpassword)
+    {
+        $sql = "UPDATE tbusuario SET senha= ?  WHERE email=?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $newpassword);
+        $stmt->bindValue(2, $email);
+
+        $stmt->execute();
+
+
+    }
+
     public function getUserByUserAndPass($usuario, $senha)
     {
         $sql = "SELECT id_usuario, nomeCompleto FROM tbusuario WHERE nome=? AND senha= ? ";
